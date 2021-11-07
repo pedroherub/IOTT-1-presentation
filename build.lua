a6032daffe496efdda85c788b343f8232b19bd1d
Pedro Hernández <pedroherub@protonmail.com>
2021-11-07 02:04:37 +0100

    Coffeemaker image in front page

--- build.lua
+++ build.lua
@@ -14,7 +14,18 @@ docfiledir       = "doc"
 typesetexe       = "xelatex"
 typesetfiles     = {"sjtubeamermintheme.tex","sjtubeamermindevguide.tex"}
 typesetdemofiles = {"red.tex","blue.tex","my.tex"}
-typesetsuppfiles = {"esquema-general.png","head.png","plant.jpg","test.csv","testgraph.tex","ref.bib","cnlogored.pdf","enlogored.pdf", "architecture.png"}
+typesetsuppfiles = {
+                        "coffeemaker.jpeg",
+                        "esquema-general.png",
+                        "head.png",
+                        "plant.jpg",
+                        "test.csv",
+                        "testgraph.tex",
+                        "ref.bib",
+                        "cnlogored.pdf",
+                        "enlogored.pdf", 
+                        "architecture.png"
+                    }
 
 testfiledir      = "test"
 checkengines     = {"xetex"}
